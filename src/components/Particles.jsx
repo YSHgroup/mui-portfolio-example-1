6de0d563b01c62f76186af83d6db94357c788be7
YSHgroup <david.dev100@outlook.com>
2023-12-03 00:04:36 -0500

    depracated particles library replace with react-particles

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,56 +1,94 @@
-import React from "react";
-import { Particles as ReactParticles } from "react-particles-js";
+import { useCallback } from "react";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
-const particlesCanvas = {
-    position: "fixed",
-    opacity: "0.3"
-  }
+const ParticlesApp = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
 
-const Particles = () => {
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
 
-  return (
-    <ReactParticles
-      canvasClassName={particlesCanvas}
-      params={{
-        particles: {
-          number: {
-            value: 45,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 1,
-              color: "tomato",
-            },
-          },
-          size: {
-            value: 8,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 6,
-              size_min: 0,
-              sync: true,
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-        },
-      }}
-    />
-  );
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
 };
 
-export default Particles;
+export default ParticlesApp
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
-// import Particles from '../components/Particles.jsx'
+import Particles from '../components/Particles.jsx'
 
 export default function Home() {
   return (
     <>
         <Header/>
-        {/* <Particles /> */}
+        <Particles />
     </>
   )
 }
